a9827e7db9af78d90ddea0651851bbf4f8e94ed8
Muhammad Saadan <msaadan.bese21seecs@seecs.edu.pk>
2024-03-01 10:48:13 +0000

    Query

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,19 +1,19 @@
 from datetime import datetime, timedelta, timezone
-from lib.db.py import pool
+from lib.db import pool, query_wrap_array
 
 class HomeActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     span.set_attribute("app.now", now.isoformat())
 
-    sql = """ SELECT * FROM activities;"""
+    sql = query_wrap_array(""" SELECT * FROM activities""")
 
     with pool.connection() as conn:
       with conn.cursor() as cur:
         cur.execute(sql)
         # this will return a tuple
         # the first field being the data
-        json = cur.fetchall()
+        json = cur.fetchone()
     return json[0]
     
     
